2cacdca1965067b998cb886d7d6e5f5ef7cd338c
Patrick José Pereira <patrickelectric@gmail.com>
2018-05-19 15:12:53 -0300

    Waterfall: Add doxygen
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/waterfall.h
+++ src/waterfall.h
@@ -9,22 +9,63 @@ Q_DECLARE_LOGGING_CATEGORY(waterfall)
 class Waterfall : public QQuickPaintedItem
 {
     Q_OBJECT
-    Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
+
+    /**
+     * @brief Return waterfall image
+     *
+     * @return QImage
+     */
     QImage image() {return _image;}
+    Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
 
-    Q_PROPERTY(float mouseDepth READ mouseDepth NOTIFY mouseDepthChanged)
+    /**
+     * @brief Get depth from mouse position
+     *
+     * @return float
+     */
     float mouseDepth() {return _mouseDepth;}
-    Q_PROPERTY(float mouseStrength READ mouseStrength NOTIFY mouseStrengthChanged)
+    Q_PROPERTY(float mouseDepth READ mouseDepth NOTIFY mouseDepthChanged)
+
+    /**
+     * @brief Get signal strength from mouse position
+     *
+     * @return float
+     */
     float mouseStrength() {return _mouseStrength;}
+    Q_PROPERTY(float mouseStrength READ mouseStrength NOTIFY mouseStrengthChanged)
 
-    Q_PROPERTY(QString theme READ theme WRITE setTheme NOTIFY themeChanged)
+    /**
+     * @brief Get theme name used in the waterfall
+     *  Check WaterfallGradient
+     *
+     * @return const QString
+     */
     const QString theme() {return _theme;}
-    Q_PROPERTY(QStringList themes READ themes NOTIFY themesChanged)
-    const QStringList themes() {return _themes;}
+    Q_PROPERTY(QString theme READ theme WRITE setTheme NOTIFY themeChanged)
 
-    Q_PROPERTY(bool smooth READ smooth WRITE setSmooth NOTIFY smoothChanged)
+    /**
+     * @brief Get all themes available
+     *
+     * @return const QStringList
+     */
+    const QStringList themes() {return _themes;}
+    Q_PROPERTY(QStringList themes READ themes NOTIFY themesChanged)
+
+    /**
+     * @brief Check if smooth is on
+     *
+     * @return true
+     * @return false
+     */
     bool smooth() {return _smooth;}
+
+    /**
+     * @brief Set the smooth state
+     *
+     * @param smooth
+     */
     void setSmooth(bool smooth) {_smooth = smooth; setAntialiasing(_smooth); emit smoothChanged();}
+    Q_PROPERTY(bool smooth READ smooth WRITE setSmooth NOTIFY smoothChanged)
 
     QList<WaterfallGradient> _gradients;
     WaterfallGradient _gradient;
@@ -50,14 +91,75 @@ signals:
     void smoothChanged();
 
 public:
+    /**
+     * @brief Construct a new Waterfall object
+     *
+     * @param parent
+     */
     Waterfall(QQuickItem *parent = 0);
+
+    /**
+     * @brief This is used by the qml pain event
+     *  This paint the waterfall in qml
+     *
+     * @param painer
+     */
     void paint(QPainter *painer);
+
+    /**
+     * @brief Set the waterfall Image
+     *
+     * @param image
+     */
     void setImage(const QImage &image);
+
+    /**
+     * @brief Set all gradients used for the themes
+     *
+     */
     void setGradients();
+
+    /**
+     * @brief Change the theme used in the waterfall
+     *
+     * @param theme name
+     */
     void setTheme(const QString& theme);
+
+    /**
+     * @brief Transform a power value 0-1 to color
+     *
+     * @param point
+     * @return QColor
+     */
     QColor valueToRGB(float point);
+
+    /**
+     * @brief Transform color to a power value
+     *
+     * @param color
+     * @return float
+     */
     float RGBToValue(const QColor& color);
+
+    /**
+     * @brief Draw a list of points in the waterfall
+     *
+     * @param points
+     */
     Q_INVOKABLE void draw(const QList<double>& points);
+
+    /**
+     * @brief Function that deals when the mouse is inside the waterfall
+     *
+     * @param event
+     */
     void hoverMoveEvent(QHoverEvent *event);
+
+    /**
+     * @brief Function that deals when the mouse is outside the waterfall
+     *
+     * @param event
+     */
     void hoverLeaveEvent(QHoverEvent *event);
 };
