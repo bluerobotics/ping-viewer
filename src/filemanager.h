c5982ff3d6218166ea251c2b71cb40cbb2e2dc17
Patrick José Pereira <patrickelectric@gmail.com>
2018-06-22 10:47:40 -0300

    FileManager: Add docstring
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/filemanager.h
+++ src/filemanager.h
@@ -8,10 +8,17 @@
 
 Q_DECLARE_LOGGING_CATEGORY(FILEMANAGER);
 
+/**
+ * @brief Manage folder creation, structure and filenames
+ *
+ */
 class FileManager : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Manage file types
+     */
     enum FileType {
         LOG,
         PICTURE,
@@ -19,18 +26,40 @@ public:
     };
     Q_ENUM(FileType)
 
-    ~FileManager();
+    /**
+     * @brief Return FileManager singleton pointer
+     *
+     * @return FileManager*
+     */
     static FileManager* self();
+    ~FileManager();
 
+    /**
+     * @brief Return filename from type of file
+     *
+     * @param type
+     * @return QString
+     */
     Q_INVOKABLE QString createFileName(FileManager::FileType type);
 
 private:
-    FileManager* operator = (FileManager& other) = delete;
-    FileManager(const FileManager& other) = delete;
+    /**
+     * @brief Construct a new File Manager object
+     *
+     */
     FileManager();
+    FileManager(const FileManager& other) = delete;
+    FileManager* operator = (FileManager& other) = delete;
 
+    /**
+     * @brief Filename structure
+     */
     const QString _fileName{QStringLiteral("yyyyMMdd-hhmmsszzz")};
 
+    /**
+     * @brief Structure to check folder existence
+     *
+     */
     struct folder {
         QDir dir;
         bool ok = false;
@@ -42,16 +71,23 @@ private:
     folder _picturesDir;
     folder _sensorLogDir;
 
+    /**
+     * @brief Manage file extensions
+     *
+     */
     const QMap<FileType, QString> fileTypeExtension {
         {LOG, ".txt"}
         , {PICTURE, ".png"}
         , {BINARY, ".bin"}
     };
 
+    /**
+     * @brief Manage file directories
+     *
+     */
     const QMap<FileType, folder*> fileTypeFolder {
         {LOG, &_guiLogDir}
         , {PICTURE, &_picturesDir}
         , {BINARY, &_sensorLogDir}
     };
-
 };
