e68e150c0ac035642ffbdd07ce8083066c955973
Patrick José Pereira <patrickelectric@gmail.com>
2018-06-22 10:50:44 -0300

    Sensor: Add docstring
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/sensor/sensor.h
+++ src/sensor/sensor.h
@@ -4,6 +4,10 @@
 #include "parsers/parser.h"
 
 // TODO rename to Device?
+/**
+ * @brief Manage sensor connection
+ *
+ */
 class Sensor : public QObject
 {
     Q_OBJECT
@@ -11,21 +15,60 @@ public:
     Sensor();
     ~Sensor();
 
-    Q_PROPERTY(AbstractLink* link READ link NOTIFY linkUpdate)
+    /**
+     * @brief Return entry link
+     *
+     * @return AbstractLink*
+     */
     AbstractLink* link() { return _linkIn ? _linkIn->self() : nullptr; };
+    Q_PROPERTY(AbstractLink* link READ link NOTIFY linkUpdate)
 
-    Q_PROPERTY(AbstractLink* linkLog READ linkLog NOTIFY linkLogUpdate)
+    /**
+     * @brief Return log link
+     *
+     * @return AbstractLink*
+     */
     AbstractLink* linkLog() { return _linkOut ? _linkOut->self() : nullptr; };
+    Q_PROPERTY(AbstractLink* linkLog READ linkLog NOTIFY linkLogUpdate)
 
-    Q_PROPERTY(QString name READ name NOTIFY nameUpdate)
+    /**
+     * @brief Return sensor name
+     *
+     * @return QString
+     */
     QString name() { return _name; };
+    Q_PROPERTY(QString name READ name NOTIFY nameUpdate)
 
+    /**
+     * @brief Add input link
+     *
+     * @param connString Connection string defined as (int:string:arg)
+     * @param logConnString Log connection string defined as (int:string:arg)
+     */
     Q_INVOKABLE void connectLink(const QString& connString, const QString& logConnString = QString());
+
+    /**
+     * @brief Add log link
+     *
+     * @param connString Log connection string defined as (int:string:arg)
+     * @return Q_INVOKABLE connectLinkLog
+     */
     Q_INVOKABLE void connectLinkLog(const QString& connString);
 
-    Q_PROPERTY(bool autodetect READ autoDetect WRITE setAutoDetect NOTIFY autoDetectUpdate)
+    /**
+     * @brief Set auto detect
+     *
+     * @param autodetect
+     */
     void setAutoDetect(bool autodetect);
+    /**
+     * @brief Check auto detect state
+     *
+     * @return true running
+     * @return false not running
+     */
     bool autoDetect() { return _autodetect; };
+    Q_PROPERTY(bool autodetect READ autoDetect WRITE setAutoDetect NOTIFY autoDetectUpdate)
 
 protected:
     bool _autodetect;
