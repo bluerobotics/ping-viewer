b2543940604799a895ba4e455a01fd650c0a0bbf
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-07 19:33:29 -0300

    test: Add first commit with FileManager test
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/test.cpp
@@ -0,0 +1,76 @@
+#define private public
+#define protected public
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQmlEngine>
+#include <QQuickStyle>
+#include <QDebug>
+#include <QRegularExpression>
+
+#include "abstractlink.h"
+#include "filemanager.h"
+#include "linkconfiguration.h"
+#include "logger.h"
+#include "ping.h"
+#include "settingsmanager.h"
+#include "util.h"
+#include "waterfall.h"
+
+#include "test.h"
+
+// Register message enums to qml
+#include "pingmessage/pingmessage.h"
+
+void Test::initTestCase()
+{
+    FileManager::self();
+}
+
+void Test::fileManager()
+{
+    auto fileManager = FileManager::self();
+    // Check if folders exist and if it's ok
+    for(const auto& folder : fileManager->folderMap) {
+        QVERIFY2(folder->dir.exists(),
+                 qPrintable(QString("Folder does not exist: %1").arg(folder->dir.path())));
+
+        // If folder does not have extension, nothing will be created inside it
+        if(folder->extension.isEmpty()) {
+            continue;
+        }
+
+        QVERIFY2(folder->ok,
+                 qPrintable(QString("Folder exist but ok is invalid: %1").arg(folder->dir.path())));
+    }
+
+    // Create file name and check if it's valid
+    // - file name need to follow FileManager._fileName string
+    const QRegularExpression regex(QStringLiteral("[0-9]{8}-[0-9]{9}"));
+    for(const auto key : fileManager->folderMap.keys()) {
+        auto folder = fileManager->folderMap[key];
+        if(folder->extension.isEmpty()) {
+            continue;
+        }
+
+        const QString fileName = fileManager->createFileName(key);
+
+        // Check if file is inside Documents
+        QVERIFY2(fileName.contains(fileManager->_docDir.dir.absolutePath()),
+                 qPrintable(QString("File is not inside the Documents folder: %1").arg(fileName)));
+
+        // Check if file has the correct name
+        QRegularExpressionMatch match = regex.match(fileName);
+        QVERIFY2(match.hasMatch(),
+                 qPrintable(QString("File name does not follow rules of FileManager: %1").arg(fileName)));
+
+        // Check if file has the correct extension
+        QVERIFY2(fileName.contains(folder->extension),
+                 qPrintable(QString("File name does not have extension compatible with FileManager: %1").arg(fileName)));
+    }
+
+    // TODO: Populate gradients folder and test FileManager.getFilesFrom
+}
+
+QTEST_MAIN(Test)
---
+++ src/test.h
@@ -0,0 +1,9 @@
+#include <QtTest/QtTest>
+
+class Test: public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase();
+    void fileManager();
+};
