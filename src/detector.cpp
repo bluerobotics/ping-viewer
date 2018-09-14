953e13c91c7743364ba43e61110b020819157e98
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-14 15:16:25 -0300

    detector: Move class from QThread to QObject
    Ref: http://blog.debao.me/2013/08/how-to-use-qthread-in-the-right-way-part-1/
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/detector.cpp
+++ src/detector.cpp
@@ -7,6 +7,7 @@
 #include <QSerialPortInfo>
 #include <QUdpSocket>
 
+#include "abstractlinknamespace.h"
 #include "detector.h"
 #include "pingmessage/pingmessage.h"
 #include "pingmessage/pingmessage_ping1D.h"
@@ -21,123 +22,158 @@ const QStringList ProtocolDetector::_invalidSerialPortNames(
 #endif
 });
 
+ProtocolDetector::ProtocolDetector()
+{
+    _linkConfigs.append({
+        {LinkType::Udp, {"192.168.2.2", "9000"}, "BlueRov2 standard connection"},
+        {LinkType::Udp, {"127.0.0.1", "1234"}, "Development port"}
+    });
+};
+
 void ProtocolDetector::scan()
 {
     _active = true;
+    LinkConfiguration linkConf;
+    // Scan until we find a ping, then stop
+    while (_active) {
+        auto linksConf = updateLinkConfigurations(_linkConfigs);
+        for(LinkConfiguration& tryLinkConf : linksConf) {
+            linkConf = tryLinkConf;
+            if(checkLink(linkConf)) {
+                break;
+            }
+        }
+        QThread::msleep(500);
+    }
+    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Scan finished.";
+}
 
+bool ProtocolDetector::checkLink(LinkConfiguration& linkConf)
+{
+    _detected = false;
+    if(linkConf.type() == LinkType::Udp) {
+        checkUdp(linkConf);
+    } else if(linkConf.type() == LinkType::Serial) {
+        checkSerial(linkConf);
+    } else {
+        qDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Couldn't handle configuration:" << linkConf;
+    }
+
+    if(_detected) {
+        qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Ping detected on:" << linkConf;
+        if(!_availableLinks.contains(linkConf)) {
+            _availableLinks.append(linkConf);
+        }
+        emit connectionDetected(linkConf);
+        _active = false;
+    }
+    return _detected;
+}
+
+QVector<LinkConfiguration> ProtocolDetector::updateLinkConfigurations(QVector<LinkConfiguration>& linkConfig) const
+{
+    QVector<LinkConfiguration> tempConfigs;
+    auto portsInfo = QSerialPortInfo::availablePorts();
+    for(const auto& portInfo : portsInfo) {
+        // Do not run with invalid ports
+        if(!isValidPort(portInfo)) {
+            continue;
+        }
+
+        // Add valid port
+        tempConfigs.append({LinkType::Serial, {portInfo.portName(), "115200"}, QString("Detector serial link")});
+    }
+    return linkConfig + tempConfigs;
+}
+
+bool ProtocolDetector::checkSerial(LinkConfiguration& linkConf)
+{
     // To find a ping, we this message on a link, then wait for a reply
     ping_msg_ping1D_empty req;
     req.set_id(Ping1DNamespace::Fw_version);
     req.updateChecksum();
 
-    // Scan until we find a ping, then stop
-    while (_active && !currentThread()->isInterruptionRequested()) {
+    QSerialPortInfo portInfo(linkConf.serialPort());
+    int baudrate = linkConf.serialBaudrate();
 
-        // Try BlueROV2 standard udp port first
-        QUdpSocket sockit;
-
-        // To test locally, change the host to 127.0.0.1 and use something like:
-        // nc -kul 127.0.0.1 8888 > /dev/ttyUSB0 < /dev/ttyUSB0
-        // or
-        // socat UDP-LISTEN:1234,fork,reuseaddr,ignoreeof FILE:/dev/ttyUSB1,b115200,raw,ignoreeof
-
-        struct HostPort {
-            QHostAddress host;
-            uint port;
-        };
-
-        for(const HostPort& hostAddress : {HostPort{QHostAddress("192.168.2.2"), 9000}, HostPort{QHostAddress("127.0.0.1"), 1234}}) {
-            qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Probing UDP" << hostAddress.host.toString() << hostAddress.port;
-            // sockit.setSocketOption(QAbstractSocket::KeepAliveOption, 1);
-            sockit.writeDatagram(reinterpret_cast<const char*>(req.msgData), req.msgDataLength(), hostAddress.host, hostAddress.port); // probe
-
-            bool detected = false;
-            int attempts = 0;
-
-            while (!detected && attempts < 10) { // Try to get a valid response, timeout after 10 * 50 ms
-                sockit.waitForReadyRead(50);
-                QNetworkDatagram datagram = sockit.receiveDatagram();
-                auto buf = datagram.data();
-                for (auto byte = buf.begin(); byte != buf.end(); ++byte) {
-                    detected = _parser.parseByte(*byte) == PingParser::NEW_MESSAGE;
-                    if (detected) {
-                        break;
-                    }
-                }
-                attempts++;
-            }
-
-            sockit.close();
-
-            if (detected) {
-                qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Ping detected on UDP";
-                emit connectionDetected(LinkType::Udp, {hostAddress.host.toString(), QString::number(hostAddress.port)});
-                _active = false;
-                return;
-            }
-        }
-
-        // Not found on UDP, now try all available serial ports
-        auto portsInfo = QSerialPortInfo::availablePorts();
-
-        // Scan all available ports
-        for (auto& portInfo : portsInfo) {
-            // Do not run with invalid ports
-            if(!isValidPort(portInfo)) {
-                continue;
-            }
-
-            // Check if port can be opened
-            if(!canOpenPort(portInfo, 500)) {
-                qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Couldn't open port" << portInfo.portName();
-                continue;
-            }
-
-            QSerialPort port(portInfo);
-
-            for (const int baudrate : {115200, 921600}) {
-                qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Probing Serial" << port.portName() << baudrate;
-
-                if(!port.open(QIODevice::ReadWrite)) {
-                    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Fail to open";
-                    break;
-                }
-                qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Port is open";
-                port.setBaudRate(baudrate);
-
-                // Probe
-                port.write(reinterpret_cast<const char*>(req.msgData), (uint16_t)req.msgDataLength());
-                port.waitForBytesWritten();
-
-                bool detected = false;
-                int attempts = 0;
-
-                while (!detected && attempts < 10) { // Try to get a valid response, timeout after 10 * 50 ms
-                    port.waitForReadyRead(50);
-                    auto buf = port.readAll();
-                    for (const auto& byte : buf) {
-                        detected = _parser.parseByte(byte) == PingParser::NEW_MESSAGE;
-                        if (detected) {
-                            break;
-                        }
-                    }
-                    attempts++;
-                }
-
-                port.close();
-
-                if (detected) {
-                    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Ping detected on" << port.portName() << baudrate;
-                    emit connectionDetected(LinkType::Serial, {port.portName(), QString::number(baudrate)});
-                    _active = false;
-                    return;
-                } else {
-                    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Couldn't detect ping";
-                }
-            }
-        }
-        msleep(500);
+    // Check if port can be opened
+    if(!canOpenPort(portInfo, 500)) {
+        qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Couldn't open port" << portInfo.portName();
+        return false;
     }
+
+    QSerialPort port(portInfo);
+
+    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Probing Serial" << port.portName() << baudrate;
+
+    if(!port.open(QIODevice::ReadWrite)) {
+        qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Fail to open";
+        return false;
+    }
+    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Port is open";
+    port.setBaudRate(baudrate);
+
+    // Probe
+    port.write(reinterpret_cast<const char*>(req.msgData), (uint16_t)req.msgDataLength());
+    port.waitForBytesWritten();
+
+    int attempts = 0;
+
+    while (!_detected && attempts < 10) { // Try to get a valid response, timeout after 10 * 50 ms
+        port.waitForReadyRead(50);
+        auto buf = port.readAll();
+        for (const auto& byte : buf) {
+            _detected = _parser.parseByte(byte) == PingParser::NEW_MESSAGE;
+            if (_detected) {
+                break;
+            }
+        }
+        attempts++;
+    }
+
+    port.close();
+
+    return _detected;
+}
+
+bool ProtocolDetector::checkUdp(LinkConfiguration& linkConf)
+{
+    // To find a ping, we this message on a link, then wait for a reply
+    ping_msg_ping1D_empty req;
+    req.set_id(Ping1DNamespace::Fw_version);
+    req.updateChecksum();
+
+    QUdpSocket socket;
+
+    // To test locally, change the host to 127.0.0.1 and use something like:
+    // nc -kul 127.0.0.1 8888 > /dev/ttyUSB0 < /dev/ttyUSB0
+    // or
+    // socat UDP-LISTEN:1234,fork,reuseaddr,ignoreeof FILE:/dev/ttyUSB1,b115200,raw,ignoreeof
+
+    qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Probing UDP:" << linkConf;
+
+    socket.writeDatagram(
+        reinterpret_cast<const char*>(req.msgData), req.msgDataLength(), QHostAddress(linkConf.udpHost()), linkConf.udpPort()
+    );
+
+    int attempts = 0;
+
+    // Try to get a valid response, timeout after 10 * 50 ms
+    while (!_detected && attempts++ < 10) {
+        socket.waitForReadyRead(50);
+        QNetworkDatagram datagram = socket.receiveDatagram();
+        auto buf = datagram.data();
+        for (auto byte : buf) {
+            _detected = _parser.parseByte(byte) == PingParser::NEW_MESSAGE;
+            if (_detected) {
+                break;
+            }
+        }
+        attempts++;
+    }
+
+    socket.close();
+    return _detected;
 }
 
 bool ProtocolDetector::canOpenPort(QSerialPortInfo& port, int msTimeout)
@@ -155,7 +191,7 @@ bool ProtocolDetector::canOpenPort(QSerialPortInfo& port, int msTimeout)
     // Wait for msTimeout
     float waitForTenthOfTimeout = 0;
     while(waitForTenthOfTimeout < 10 && !future.isFinished()) {
-        msleep(msTimeout/10.0f);
+        QThread::msleep(msTimeout/10.0f);
         qCDebug(PING_PROTOCOL_PROTOCOLDETECTOR) << "Waiting port to open.. " << waitForTenthOfTimeout << port.portName();
         waitForTenthOfTimeout += 1;
     }
@@ -167,7 +203,7 @@ bool ProtocolDetector::canOpenPort(QSerialPortInfo& port, int msTimeout)
     return ok;
 }
 
-bool ProtocolDetector::isValidPort(QSerialPortInfo& serialPortInfo)
+bool ProtocolDetector::isValidPort(const QSerialPortInfo& serialPortInfo) const
 {
     for(const auto& name : _invalidSerialPortNames) {
         if(serialPortInfo.portName().contains(name, Qt::CaseInsensitive)) {
--- src/detector.h
+++ src/detector.h
@@ -3,37 +3,56 @@
 #include <QThread>
 
 #include "abstractlink.h"
+#include "linkconfiguration.h"
 #include "parsers/parser_ping.h"
 
 class QSerialPortInfo;
 
 /// This class will scan network ports and serial ports for a ping device
 /// TODO subclass and support discovery of other protocols/devices
-class ProtocolDetector : public QThread
+class ProtocolDetector : public QObject
 {
     Q_OBJECT
 public:
-    ProtocolDetector()
+    ProtocolDetector();
+
+    void appendConfiguration(LinkConfiguration& linkConfig)
     {
-        connect(this, &QThread::finished, this, [this] { _active = false; });
+        _linkConfigs.append(linkConfig);
+    }
+
+    QVector<LinkConfiguration> availableLinks()
+    {
+        return _availableLinks;
     };
+
+    bool checkLink(LinkConfiguration& linkConf);
 
     static const QStringList& invalidSerialPortNames()
     {
         return _invalidSerialPortNames;
     };
-    bool isValidPort(QSerialPortInfo& serialPortInfo);
+    bool isValidPort(const QSerialPortInfo& serialPortInfo) const;
+    bool isRunning() const { return _active; };
+    void stop() { _active = false; };
+
+public slots:
     void scan();
 
 signals:
-    void connectionDetected(AbstractLinkNamespace::LinkType connType, QStringList config); // Todo can we send a pre-configured, pre-connected link object with the signal?
+    void connectionDetected(LinkConfiguration linkConf);
 
 protected:
-    void run() { scan(); }
     bool canOpenPort(QSerialPortInfo& port, int msTimeout);
+    bool checkSerial(LinkConfiguration& linkConf);
+    bool checkUdp(LinkConfiguration& linkConf);
+    QVector<LinkConfiguration> updateLinkConfigurations(QVector<LinkConfiguration>& linkConfig) const;
 
 private:
     bool _active { false };
+    bool _detected { false };
+    QVector<LinkConfiguration> _availableLinks;
+    QVector<LinkConfiguration> _linkConfigs;
     PingParser _parser;
     static const QStringList _invalidSerialPortNames;
 };
