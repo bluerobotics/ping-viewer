af539daccbaa6f0fd3e8d0e8ac325ad1b4094641
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:47:28 -0300

    logthread: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/logthread.h
+++ src/link/logthread.h
@@ -5,22 +5,81 @@
 #include <QTimer>
 #include <QVector>
 
+/**
+ * @brief Play sensor logs
+ *
+ */
 class LogThread : public QTimer
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Construct a new Log Thread object
+     *
+     * @param parent
+     */
     LogThread(QObject *parent = nullptr);
+
+    /**
+     * @brief Destroy the Log Thread object
+     *
+     */
     ~LogThread();
 
-    void startJob() { _playLog = true; start();};
-    void pauseJob() { _playLog = false; };
-    int packageSize() { return _log.size() - 1; };
-    int packageIndex() { return _logIndex; }
-    void setPackageIndex(int index) { if(_logIndex < _log.size()) _logIndex = index; }
-    QTime totalTime();
+    /**
+     * @brief Append new log message
+     *
+     * @param time
+     * @param data
+     */
+    void append(QTime time, QByteArray data) { _log.append(Pack{time, data}); };
+
+    /**
+     * @brief Return log elapsed time
+     *
+     * @return QTime
+     */
     QTime elapsedTime();
 
-    void append(QTime time, QByteArray data) { _log.append(Pack{time, data}); };
+    /**
+     * @brief Return last package index
+     *
+     * @return int
+     */
+    int packageIndex() { return _logIndex; }
+
+    /**
+     * @brief Return package size
+     *
+     * @return int
+     */
+    int packageSize() { return _log.size() - 1; };
+
+    /**
+     * @brief Pause log
+     *
+     */
+    void pauseJob() { _playLog = false; };
+
+    /**
+     * @brief Set the package index
+     *
+     * @param index
+     */
+    void setPackageIndex(int index) { if(_logIndex < _log.size()) _logIndex = index; }
+
+    /**
+     * @brief Start playing log
+     *
+     */
+    void startJob() { _playLog = true; start();};
+
+    /**
+     * @brief Return total time of log
+     *
+     * @return QTime
+     */
+    QTime totalTime();
 
 signals:
     void newPackage(const QByteArray& data);
