0aefe7257f0de23cc3dd9419a124a7f848ac8949
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:51:30 -0300

    test: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test.h
+++ src/test.h
@@ -1,12 +1,40 @@
 #include <QtTest/QtTest>
 
+/**
+ * @brief Test class to test basic functionalities
+ *
+ */
 class Test: public QObject
 {
     Q_OBJECT
 private slots:
+    /**
+     * @brief Initialize what is necessary
+     *
+     */
     void initTestCase();
+
+    /**
+     * @brief Test file manager
+     *
+     */
     void fileManager();
+
+    /**
+     * @brief Test ring vector
+     *
+     */
     void ringVector();
+
+    /**
+     * @brief Test settings manager
+     *
+     */
     void settingsManager();
+
+    /**
+     * @brief Test waterfall gradient
+     *
+     */
     void waterfallGradient();
 };
