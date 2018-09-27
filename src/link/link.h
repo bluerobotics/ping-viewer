26f105b6613537a2378930db021753301bed5a2e
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:22:18 -0300

    link: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/link.h
+++ src/link/link.h
@@ -5,15 +5,37 @@
 #include "abstractlink.h"
 #include "linkconfiguration.h"
 
+/**
+ * @brief Top class for all links
+ *
+ */
 class Link : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Construct a new Link object
+     *
+     * @param linkType
+     * @param name
+     * @param parent
+     */
     Link(LinkType linkType = LinkType::None, QString name = QString(), QObject* parent = nullptr);
+
+    /**
+     * @brief Construct a new Link object
+     *
+     * @param linkConfiguration
+     * @param parent
+     */
     Link(const LinkConfiguration& linkConfiguration, QObject* parent = nullptr);
     ~Link();
 
-    //This will handle the connection change
+    /**
+     * @brief This will handle the link used
+     *
+     * @return AbstractLink*
+     */
     AbstractLink* self() { return _abstractLink; };
 
 private:
