0de05ab7163efe42582439621cd7bd7063105af3
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:48:45 -0300

    seriallink: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/seriallink.h
+++ src/link/seriallink.h
@@ -4,20 +4,79 @@
 
 #include "abstractlink.h"
 
+/**
+ * @brief Serial Link connection class
+ *
+ */
 class SerialLink : public AbstractLink
 {
-
 public:
+    /**
+     * @brief Construct a new Serial Link object
+     *
+     * @param parent
+     */
     SerialLink(QObject* parent = nullptr);
+
+    /**
+     * @brief Destroy the Serial Link object
+     *
+     */
     ~SerialLink();
 
-    bool isOpen() final { return _port->isWritable() && _port->isReadable(); };
-    bool setConfiguration(const LinkConfiguration& linkConfiguration) final;
-    bool startConnection() final { return _port->open(QIODevice::ReadWrite); };
-    bool finishConnection() final;
+    /**
+     * @brief Human friendly error string
+     *
+     * @return QString
+     */
     QString errorString() final { return _port->errorString(); };
+
+    /**
+     * @brief Finish connection
+     *
+     * @return true
+     * @return false
+     */
+    bool finishConnection() final;
+
+    /**
+     * @brief Check if connection is open
+     *
+     * @return true
+     * @return false
+     */
+    bool isOpen() final { return _port->isWritable() && _port->isReadable(); };
+
+    /**
+     * @brief Return a list of all available connections
+     *
+     * @return QStringList
+     */
     QStringList listAvailableConnections() final;
+
+    /**
+     * @brief Return a list of available ports
+     *
+     * @return QSerialPort*
+     */
     QSerialPort* port() { return _port; };
+
+    /**
+     * @brief Set link configuration
+     *
+     * @param linkConfiguration
+     * @return true
+     * @return false
+     */
+    bool setConfiguration(const LinkConfiguration& linkConfiguration) final;
+
+    /**
+     * @brief Start connection and check if is ready to go
+     *
+     * @return true
+     * @return false
+     */
+    bool startConnection() final { return _port->open(QIODevice::ReadWrite); };
 
 private:
     QSerialPort* _port;
