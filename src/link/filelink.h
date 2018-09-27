3da8dc6c24fcec5d76653656ac32a07022e8041f
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:22:08 -0300

    filelink: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/filelink.h
+++ src/link/filelink.h
@@ -9,27 +9,122 @@
 #include "abstractlink.h"
 #include "logthread.h"
 
+/**
+ * @brief File connection class
+ *
+ */
 class FileLink : public AbstractLink
 {
 public:
+    /**
+     * @brief Construct a new File Link object
+     *
+     * @param parent
+     */
     FileLink(QObject* parent = nullptr);
     ~FileLink();
 
-    bool isOpen() final;
-    bool setConfiguration(const LinkConfiguration& linkConfiguration) final;
-    bool startConnection() final;
-    bool finishConnection() final;
+    /**
+     * @brief Return size of file in bytes
+     *
+     * @return qint64
+     */
+    qint64 byteSize() final { return _file.bytesAvailable(); };
+
+    /**
+     * @brief Return elapsed time of log
+     *
+     * @return QTime
+     */
+    QTime elapsedTime() final { return _logThread ? _logThread->elapsedTime() : QTime(); };
+
+    /**
+     * @brief Return a human friendly error message
+     *
+     * @return QString
+     */
     QString errorString() final { return _file.errorString(); };
 
+    /**
+     * @brief Closes connection
+     *
+     * @return true
+     * @return false
+     */
+    bool finishConnection() final;
+
+    /**
+     * @brief Check if connection is open
+     *
+     * @return true
+     * @return false
+     */
+    bool isOpen() final;
+
+    /**
+     * @brief Check if connection is writable
+     *
+     * @return true
+     * @return false
+     */
     bool isWritable() final { return false; };
-    void start() final { if(_logThread) _logThread->startJob(); };
-    void pause() final { if(_logThread) _logThread->pauseJob(); };
-    qint64 byteSize() final { return _file.bytesAvailable(); };
-    int packageSize() final { return _logThread ? _logThread->packageSize() : 0; };
+
+    /**
+     * @brief Return package index
+     *
+     * @return int
+     */
     int packageIndex() final { return _logThread ? _logThread->packageIndex() : 0; };
+
+    /**
+     * @brief Return number of packages
+     *
+     * @return int
+     */
+    int packageSize() final { return _logThread ? _logThread->packageSize() : 0; };
+
+    /**
+     * @brief Pause log
+     *
+     */
+    void pause() final { if(_logThread) _logThread->pauseJob(); };
+
+    /**
+     * @brief Set the configuration object
+     *
+     * @param linkConfiguration
+     * @return true
+     * @return false
+     */
+    bool setConfiguration(const LinkConfiguration& linkConfiguration) final;
+
+    /**
+     * @brief Set the package index
+     *
+     * @param index
+     */
     void setPackageIndex(int index) { if(_logThread) _logThread->setPackageIndex(index); }
+
+    /**
+     * @brief Start log
+     *
+     */
+    void start() final { if(_logThread) _logThread->startJob(); };
+
+    /**
+     * @brief Start connection
+     *
+     * @return true
+     * @return false
+     */
+    bool startConnection() final;
+
+    /**
+     * @brief Return log total time
+     *
+     * @return QTime
+     */
     QTime totalTime() final { return _logThread ? _logThread->totalTime() : QTime(); };
-    QTime elapsedTime() final { return _logThread ? _logThread->elapsedTime() : QTime(); };
 
 private:
     struct Pack {
