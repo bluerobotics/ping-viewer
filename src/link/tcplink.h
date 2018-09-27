66ed38f70a2ae97b08630621b95ab552763c68d0
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-27 17:50:58 -0300

    tcplink: Fix doxygen documentation
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/tcplink.h
+++ src/link/tcplink.h
@@ -2,9 +2,23 @@
 
 #include "abstractlink.h"
 
+/**
+ * @brief TCP connection class
+ *
+ */
 class TCPLink : public AbstractLink
 {
 public:
+    /**
+     * @brief Construct a new TCPLink object
+     *
+     * @param parent
+     */
     TCPLink(QObject* parent = nullptr);
+
+    /**
+     * @brief Destroy the TCPLink object
+     *
+     */
     ~TCPLink();
 };
