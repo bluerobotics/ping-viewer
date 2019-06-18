53f2b4c941ec9ba96dfbcfc49f660b4d6bc548a3
Jacob Walser <jwalser90@gmail.com>
2019-06-18 08:40:51 -0400

    parser: move code out of submodule, PingParserExt

---
+++ src/sensor/parser-ping.cpp
@@ -0,0 +1,34 @@
+#include "parser-ping.h"
+
+void PingParserExt::clearBuffer()
+{
+    _parser.reset();
+}
+
+void PingParserExt::parseBuffer(const QByteArray& data)
+{
+    for(int i = 0; i < data.length(); i++) {
+        PingParser::ParseState state = _parser.parseByte(data.at(i));
+        if (state == PingParser::ParseState::NEW_MESSAGE) {
+            parsed++;
+            _rxMessage = _parser.rxMessage;
+            emit newMessage(_rxMessage);
+        } else if (state == PingParser::ParseState::ERROR) {
+            errors++;
+            emit parseError();
+        }
+    }
+}
+
+Parser::ParserState PingParserExt::parseByte(const char byte)
+{
+    PingParser::ParseState state = _parser.parseByte(byte);
+    if (state == PingParser::ParseState::NEW_MESSAGE) {
+        _rxMessage = _parser.rxMessage;
+        return Parser::ParserState::NEW_MESSAGE;
+    } else if (state == PingParser::ERROR) {
+        return Parser::ParserState::ERROR;
+    }
+
+    return Parser::ParserState::PARSING;
+}
---
+++ src/sensor/parser.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <QObject>
+#include "ping-message.h"
+
+/**
+ * This class digests data and notifies owner when something interesting happens
+ */
+class Parser : public QObject
+{
+    Q_OBJECT
+public:
+    Parser() = default;
+    ~Parser() = default;
+
+    /**
+     * @brief parser states
+     */
+    enum ParserState {
+        ERROR, // got some unexpected data
+        PARSING, // got some expected data, feed me more
+        NEW_MESSAGE // got a new packet
+    };
+
+    uint32_t parsed = 0; // number of messages/packets successfully parsed
+    uint32_t errors = 0; // number of parse errors
+
+    /**
+     * @brief clear parse state
+     */
+    virtual void clearBuffer() = 0;
+
+    /**
+     * @brief asynchronous use, Child classes should signal when something happens ie. 'emit newMessage(Message m)'
+     * @param data the next sequence of bytes in the serial stream being parsed
+     */
+    virtual void parseBuffer(const QByteArray& data) = 0;
+
+    /**
+     * @brief synchronous use, Child should return flags indicating incremental parse result/status
+     * @param byte the next byte in serial stream being parsed
+     * @return the current state of the parser
+     */
+    virtual ParserState parseByte(const char byte) = 0;
+
+    /**
+     * @brief Access the last message that was parsed for synchronous use
+     *
+     * @return the last message that was parsed
+     */
+    ping_message rxMessage() const { return _rxMessage; }
+
+signals:
+    void newMessage(const ping_message& msg);
+    void parseError();
+
+protected:
+    ping_message _rxMessage;
+};
