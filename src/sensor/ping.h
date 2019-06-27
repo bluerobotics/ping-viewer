7c5f97bfa27fd3f2745602324281d479004007b2
Patrick José Pereira <patrickelectric@gmail.com>
2019-06-27 16:45:59 -0300

    ping: Remove old _lostMessages
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/sensor/ping.h
+++ src/sensor/ping.h
@@ -511,9 +511,6 @@ private:
         },
     };
 
-    // total of lost messages
-    int _lostMessages = 0;
-
     struct messageStatus {
         // Requested and acknowledge
         int ack = 0;
