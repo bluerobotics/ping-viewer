9a69b393396cbdb4f521fb44b4ce6f32d935e3ab
Patrick José Pereira <patrickelectric@gmail.com>
2019-07-05 10:56:20 -0300

    udplink: Fix socket behaviour, connect as a client
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/udplink.cpp
+++ src/link/udplink.cpp
@@ -13,11 +13,11 @@ UDPLink::UDPLink(QObject* parent)
     setType(LinkType::Udp);
 
     connect(_udpSocket, &QIODevice::readyRead, this, [this]() {
-        emit newData(_udpSocket->receiveDatagram().data());
+        emit newData(_udpSocket->readAll());
     });
 
     connect(this, &AbstractLink::sendData, this, [this](const QByteArray& data) {
-        _udpSocket->writeDatagram(data, _hostAddress, _port);
+        _udpSocket->write(data);
     });
 }
 
@@ -34,6 +34,28 @@ bool UDPLink::setConfiguration(const LinkConfiguration& linkConfiguration)
 
     _hostAddress = QHostAddress(linkConfiguration.args()->at(0));
     _port = linkConfiguration.args()->at(1).toInt();
+
+    // Check protocol detector comments and documentation about correct connect procedure
+    // TODO: Maybe UDPLink should provide a static function unify server connection/test
+
+    // Bind port
+    _udpSocket->bind(QHostAddress::Any, _port, QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);
+
+    // Connect with server
+    _udpSocket->connectToHost(_hostAddress, _port);
+
+    // Give the socket a second to connect to the other side otherwise error out
+    int socketAttemps = 0;
+    while(!_udpSocket->waitForConnected(100) && socketAttemps < 10 ) {
+        // Increases socketAttemps here to avoid empty loop optimization
+        socketAttemps++;
+    }
+    if(_udpSocket->state() != QUdpSocket::ConnectedState) {
+        qCWarning(PING_PROTOCOL_UDPLINK) << "Socket is not in connected state.";
+        QString errorMessage = QStringLiteral("Error (%1): %2.").arg(_udpSocket->state()).arg(_udpSocket->errorString());
+        qCWarning(PING_PROTOCOL_UDPLINK) << errorMessage;
+        return false;
+    }
 
     return true;
 }
