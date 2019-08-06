5c1ccd042ddbed6c6c3edf71af6c74221a2f8260
Patrick José Pereira <patrickelectric@gmail.com>
2019-08-06 11:14:04 -0300

    sensorinfo: First commit
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/link/sensorinfo.cpp
@@ -0,0 +1,51 @@
+#include <QDataStream>
+#include <QDebug>
+
+#include "sensorinfo.h"
+
+QDebug operator<<(QDebug out, const SensorFamily& other)
+{
+    QString string;
+    switch(other) {
+    case SensorFamily::UNKNOWN:
+        string = "UNKNOWN";
+        break;
+    case SensorFamily::PING:
+        string = "PING";
+        break;
+    default:
+        string = "INVALID";
+        break;
+    }
+    return out << string;
+}
+
+QDataStream& operator<<(QDataStream& out, const SensorFamily& other)
+{
+    return out << static_cast<int>(other);
+}
+
+QDataStream& operator>>(QDataStream& in, SensorFamily& other)
+{
+    int value;
+    in >> value;
+    other = static_cast<SensorFamily>(value);
+    return in;
+}
+
+QDebug operator<<(QDebug out, const SensorInfo& other)
+{
+    QString string;
+    switch(other.family) {
+    case SensorFamily::UNKNOWN:
+        string = "UNKNOWN";
+        break;
+    case SensorFamily::PING:
+        string = PingHelper::nameFromDeviceType(other.type.ping);
+        break;
+    default:
+        string = "INVALID";
+        break;
+    }
+    return out << other.family << string;
+}
