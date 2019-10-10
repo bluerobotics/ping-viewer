1aa3cccf972cf27661c1e075f0fc17bd6929d9b2
Patrick José Pereira <patrickelectric@gmail.com>
2019-10-10 10:27:12 -0300

    clang-format: Replace astyle script
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ tools/clang-format.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# Functions
+echob() {
+    echo "${bold}${1}${normal}"
+}
+
+echob "Running $(clang-format --version)..."
+
+# Clang-format
+# More information: https://clang.llvm.org/docs/ClangFormatStyleOptions.html
+
+clang-format ${PWD}/src/**/*.{cpp,h} -i --verbose
+echob "Thank you for running it !"
