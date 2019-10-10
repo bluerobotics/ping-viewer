d2cf58c5c836513bde210d42369a928943a55931
Patrick José Pereira <patrickelectric@gmail.com>
2019-10-10 12:47:42 -0300

    settings: Use clang-format
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/settings/qjsonsettings.cpp
+++ src/settings/qjsonsettings.cpp
@@ -4,24 +4,24 @@
 #include <QFile>
 #include <QIODevice>
 
-QJsonSettings::QJsonSettings(QObject *parent)
+QJsonSettings::QJsonSettings(QObject* parent)
     : QAbstractListModel(parent)
 {
 }
 
-QJsonSettings::QJsonSettings(const QString& fileName, QObject *parent)
+QJsonSettings::QJsonSettings(const QString& fileName, QObject* parent)
     : QAbstractListModel(parent)
 {
     load(fileName);
 }
 
-QJsonSettings::QJsonSettings(QIODevice * device, QObject *parent)
+QJsonSettings::QJsonSettings(QIODevice* device, QObject* parent)
     : QAbstractListModel(parent)
 {
     load(device);
 }
 
-QJsonSettings::QJsonSettings(const QByteArray& json, QObject *parent)
+QJsonSettings::QJsonSettings(const QByteArray& json, QObject* parent)
     : QAbstractListModel(parent)
 {
     loadJson(json);
@@ -29,7 +29,7 @@ QJsonSettings::QJsonSettings(const QByteArray& json, QObject *parent)
 
 QJsonSettings::~QJsonSettings() = default;
 
-bool QJsonSettings::load(const QString &fileName)
+bool QJsonSettings::load(const QString& fileName)
 {
     QFile file(fileName);
     bool success = false;
@@ -41,16 +41,13 @@ bool QJsonSettings::load(const QString &fileName)
     return success;
 }
 
-bool QJsonSettings::load(QIODevice *device)
-{
-    return loadJson(device->readAll());
-}
+bool QJsonSettings::load(QIODevice* device) { return loadJson(device->readAll()); }
 
-bool QJsonSettings::loadJson(const QByteArray &json)
+bool QJsonSettings::loadJson(const QByteArray& json)
 {
     _jsonDocument = QJsonDocument::fromJson(json);
 
-    if(_jsonDocument.isNull()) {
+    if (_jsonDocument.isNull()) {
         qCritical() << "Failed to load json settings!";
         qDebug().noquote() << json;
         return false;
@@ -58,20 +55,20 @@ bool QJsonSettings::loadJson(const QByteArray &json)
 
     _rootValue = _jsonDocument[_mainKey];
 
-    if(!_rootValue.isArray()) {
+    if (!_rootValue.isArray()) {
         qCritical() << "Settings key need to be an array!";
         return false;
     }
     return true;
 }
 
-int QJsonSettings::rowCount(const QModelIndex &parent) const
+int QJsonSettings::rowCount(const QModelIndex& parent) const
 {
     Q_UNUSED(parent)
     return _rootValue.toArray().size();
 }
 
-QVariant QJsonSettings::data(const QModelIndex &index, int role) const
+QVariant QJsonSettings::data(const QModelIndex& index, int role) const
 {
     // Role will be used in future, right now only name is used
     Q_UNUSED(role)
@@ -79,14 +76,11 @@ QVariant QJsonSettings::data(const QModelIndex &index, int role) const
     return _rootValue.toArray().at(index.row())[QString(_roles[0])].toVariant();
 }
 
-QHash<int, QByteArray> QJsonSettings::roleNames() const
-{
-    return _roles;
-}
+QHash<int, QByteArray> QJsonSettings::roleNames() const { return _roles; }
 
 QJsonObject QJsonSettings::object(int index) const
 {
-    if(index > _rootValue.toArray().size() || index < 0) {
+    if (index > _rootValue.toArray().size() || index < 0) {
         return QJsonObject();
     }
     return _rootValue.toArray().at(index).toObject();
@@ -95,8 +89,8 @@ QJsonObject QJsonSettings::object(int index) const
 QJsonObject QJsonSettings::object(const QString& key) const
 {
     int size = _rootValue.toArray().size();
-    for(int i = 0; i < size; i++) {
-        if(_rootValue.toArray().at(i)[QString(_roles[0])].toString() == key) {
+    for (int i = 0; i < size; i++) {
+        if (_rootValue.toArray().at(i)[QString(_roles[0])].toString() == key) {
             return _rootValue.toArray().at(i).toObject();
         }
     }
--- src/settings/settingsmanagerhelper.h
+++ src/settings/settingsmanagerhelper.h
@@ -7,26 +7,17 @@
 /**
  *@brief call the correct method for Qvariants that have bool values
  */
-template<bool> bool qVariantValueOf(const QVariant& variant)
-{
-    return variant.toBool();
-}
+template <bool> bool qVariantValueOf(const QVariant& variant) { return variant.toBool(); }
 
 /**
  *@brief call the correct method for Qvariants that have int values
  */
-template<int> int qVariantValueOf(const QVariant& variant)
-{
-    return variant.toInt();
-}
+template <int> int qVariantValueOf(const QVariant& variant) { return variant.toInt(); }
 
 /**
  *@brief call the correct method for Qvariants that have T values
  */
-template<typename T> T qVariantValueOf(const QVariant& variant)
-{
-    return variant.value<T>();
-}
+template <typename T> T qVariantValueOf(const QVariant& variant) { return variant.value<T>(); }
 
 /**
  * @brief Defines a simple propriety
@@ -61,31 +52,36 @@ template<typename T> T qVariantValueOf(const QVariant& variant)
  *    // Define internal variable
  *    myType _myName;
  */
-#define AUTO_PROPERTY(TYPE, NAME, DEFAULT_VALUE) \
-    Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME ## Changed ) \
-public: \
-    TYPE NAME() { \
-        QVariant variant = _settings.value(QStringLiteral(#NAME)); \
-        if(variant.isValid()) { \
-            _ ## NAME = ::qVariantValueOf<TYPE>(variant); \
-        } else { \
-            _ ## NAME = DEFAULT_VALUE; \
-            NAME(_ ## NAME); \
-        } \
-        qCDebug(SETTINGSMANAGER) << QStringLiteral("Load %1 with:").arg(#NAME) << _ ## NAME;\
-        return _ ## NAME; \
-    } \
-    void NAME(TYPE value) { \
-        if(_ ## NAME == value) { return; }\
-        _ ## NAME = value; \
-        _settings.setValue(QStringLiteral(#NAME), qVariantFromValue(value)); \
-        _settings.sync(); \
-        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;\
-        emit NAME ## Changed(); \
-    } \
-    Q_SIGNAL void NAME ## Changed();\
-private: \
-    TYPE _ ## NAME;
+#define AUTO_PROPERTY(TYPE, NAME, DEFAULT_VALUE)                                                                       \
+    Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME##Changed)                                                    \
+public:                                                                                                                \
+    TYPE NAME()                                                                                                        \
+    {                                                                                                                  \
+        QVariant variant = _settings.value(QStringLiteral(#NAME));                                                     \
+        if (variant.isValid()) {                                                                                       \
+            _##NAME = ::qVariantValueOf<TYPE>(variant);                                                                \
+        } else {                                                                                                       \
+            _##NAME = DEFAULT_VALUE;                                                                                   \
+            NAME(_##NAME);                                                                                             \
+        }                                                                                                              \
+        qCDebug(SETTINGSMANAGER) << QStringLiteral("Load %1 with:").arg(#NAME) << _##NAME;                             \
+        return _##NAME;                                                                                                \
+    }                                                                                                                  \
+    void NAME(TYPE value)                                                                                              \
+    {                                                                                                                  \
+        if (_##NAME == value) {                                                                                        \
+            return;                                                                                                    \
+        }                                                                                                              \
+        _##NAME = value;                                                                                               \
+        _settings.setValue(QStringLiteral(#NAME), qVariantFromValue(value));                                           \
+        _settings.sync();                                                                                              \
+        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;                               \
+        emit NAME##Changed();                                                                                          \
+    }                                                                                                                  \
+    Q_SIGNAL void NAME##Changed();                                                                                     \
+                                                                                                                       \
+private:                                                                                                               \
+    TYPE _##NAME;
 
 /**
  * @brief Create a model property for qml connections
@@ -117,23 +113,31 @@ private: \
  *    myType _myName;
  *    const myModelType* _myNameModel{new myModelType({model_var_1, model_var_2})};
  */
-#define AUTO_PROPERTY_MODEL(TYPE, NAME, MODEL_TYPE, MODEL_LIST) \
-    Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME ## Changed ) \
-    Q_PROPERTY(const MODEL_TYPE* NAME ## Model READ NAME ## Model CONSTANT ) \
-public: \
-    TYPE NAME() { _ ## NAME = _settings.value(QStringLiteral(#NAME)).value<TYPE>(); return _ ## NAME ; } \
-    void NAME(TYPE value) { \
-        if(_ ## NAME == value) { return; }\
-        _ ## NAME = value; \
-        _settings.setValue(QStringLiteral(#NAME), value); \
-        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;\
-        emit NAME ## Changed(); \
-    } \
-    const MODEL_TYPE* NAME ## Model() { return _ ## NAME ## Model; }\
-    Q_SIGNAL void NAME ## Changed();\
-private: \
-    TYPE _ ## NAME; \
-    const MODEL_TYPE* _ ## NAME ## Model{new MODEL_TYPE(MODEL_LIST)};
+#define AUTO_PROPERTY_MODEL(TYPE, NAME, MODEL_TYPE, MODEL_LIST)                                                        \
+    Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME##Changed)                                                    \
+    Q_PROPERTY(const MODEL_TYPE* NAME##Model READ NAME##Model CONSTANT)                                                \
+public:                                                                                                                \
+    TYPE NAME()                                                                                                        \
+    {                                                                                                                  \
+        _##NAME = _settings.value(QStringLiteral(#NAME)).value<TYPE>();                                                \
+        return _##NAME;                                                                                                \
+    }                                                                                                                  \
+    void NAME(TYPE value)                                                                                              \
+    {                                                                                                                  \
+        if (_##NAME == value) {                                                                                        \
+            return;                                                                                                    \
+        }                                                                                                              \
+        _##NAME = value;                                                                                               \
+        _settings.setValue(QStringLiteral(#NAME), value);                                                              \
+        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;                               \
+        emit NAME##Changed();                                                                                          \
+    }                                                                                                                  \
+    const MODEL_TYPE* NAME##Model() { return _##NAME##Model; }                                                         \
+    Q_SIGNAL void NAME##Changed();                                                                                     \
+                                                                                                                       \
+private:                                                                                                               \
+    TYPE _##NAME;                                                                                                      \
+    const MODEL_TYPE* _##NAME##Model {new MODEL_TYPE(MODEL_LIST)};
 
 /**
  * @brief Create a model property for qml connections with QJsonSettings
@@ -158,26 +162,33 @@ private: \
     int _myNameIndex; \
     const QJsonSettings* _myNameModel{new QJsonSettings(myJson)};
  */
-#define AUTO_PROPERTY_JSONMODEL(NAME, JSON) \
-    Q_PROPERTY(int NAME ## Index READ NAME ## Index WRITE NAME ## Index NOTIFY NAME ## Index ## Changed ) \
-    Q_PROPERTY(const QJsonSettings* NAME ## Model READ NAME ## Model CONSTANT) \
-    Q_PROPERTY(QJsonObject NAME READ NAME NOTIFY NAME ## Index ## Changed ) \
-public: \
-    int NAME ## Index() { _ ## NAME ## Index = _settings.value(QStringLiteral(#NAME)).value<int>(); return _ ## NAME ## Index ; } \
-    void NAME ## Index(int value) { \
-        if(_ ## NAME ## Index == value) { return; }\
-        _ ## NAME ## Index = value; \
-        _settings.setValue(QStringLiteral(#NAME), value); \
-        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;\
-        emit NAME ## Index ## Changed(); \
-    } \
-    const QJsonSettings* NAME ## Model() { return _ ## NAME ## Model; }\
-    QJsonObject NAME() { return _ ## NAME ## Model->object(NAME ## Index()); } \
-    Q_SIGNAL void NAME ## Index ## Changed();\
-private: \
-    int _ ## NAME ## Index; \
-    const QJsonSettings* _ ## NAME ## Model{new QJsonSettings(JSON)};
+#define AUTO_PROPERTY_JSONMODEL(NAME, JSON)                                                                            \
+    Q_PROPERTY(int NAME##Index READ NAME##Index WRITE NAME##Index NOTIFY NAME##Index##Changed)                         \
+    Q_PROPERTY(const QJsonSettings* NAME##Model READ NAME##Model CONSTANT)                                             \
+    Q_PROPERTY(QJsonObject NAME READ NAME NOTIFY NAME##Index##Changed)                                                 \
+public:                                                                                                                \
+    int NAME##Index()                                                                                                  \
+    {                                                                                                                  \
+        _##NAME##Index = _settings.value(QStringLiteral(#NAME)).value<int>();                                          \
+        return _##NAME##Index;                                                                                         \
+    }                                                                                                                  \
+    void NAME##Index(int value)                                                                                        \
+    {                                                                                                                  \
+        if (_##NAME##Index == value) {                                                                                 \
+            return;                                                                                                    \
+        }                                                                                                              \
+        _##NAME##Index = value;                                                                                        \
+        _settings.setValue(QStringLiteral(#NAME), value);                                                              \
+        qCDebug(SETTINGSMANAGER) << QStringLiteral("Save %1 with:").arg(#NAME) << value;                               \
+        emit NAME##Index##Changed();                                                                                   \
+    }                                                                                                                  \
+    const QJsonSettings* NAME##Model() { return _##NAME##Model; }                                                      \
+    QJsonObject NAME() { return _##NAME##Model->object(NAME##Index()); }                                               \
+    Q_SIGNAL void NAME##Index##Changed();                                                                              \
+                                                                                                                       \
+private:                                                                                                               \
+    int _##NAME##Index;                                                                                                \
+    const QJsonSettings* _##NAME##Model {new QJsonSettings(JSON)};
 
 // Wrap model list
-#define MODEL(...) \
-    __VA_ARGS__
+#define MODEL(...) __VA_ARGS__
