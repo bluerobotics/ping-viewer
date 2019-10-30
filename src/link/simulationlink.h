267ec846d76bf2a470db6d490ab5b30f90160737
Patrick José Pereira <patrickelectric@gmail.com>
2019-10-30 09:47:27 -0700

    simulationlink: Add randomPoint function
    This is the base point for basic simulations,
    for the actual software architecture this is the best
    to add such thing.
    A future sensor simulation abstract class may help
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/simulationlink.h
+++ src/link/simulationlink.h
@@ -3,6 +3,8 @@
 #include "abstractlink.h"
 #include "pingparserext.h"
 
+#include <QRandomGenerator>
+
 /**
  * @brief Simulation connection class
  *
@@ -23,4 +25,15 @@ public:
      * @return false
      */
     bool isWritable() final { return false; };
+
+protected:
+    /**
+     * @brief Limit the random generation to the interval of max and min possible values of T
+     *
+     * @return float random value
+     */
+    template <typename T> float randomPoint() const
+    {
+        return QRandomGenerator::global()->bounded(std::numeric_limits<T>::max() + 1);
+    };
 };
