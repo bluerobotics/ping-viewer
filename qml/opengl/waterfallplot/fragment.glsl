patrickelectric/ping-components-1

opengl: Add waterfallplot
Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

qml/opengl/waterfallplot/fragment.glsl
@@ -1,0 +1,9 @@
+varying vec2 coord;
+uniform float ratio;
+uniform sampler2D src;
+
+void main() {
+    // Mod is used to wrap around the 0-1 scaled x axis
+    vec2 shiftedCoord = mod(coord + vec2(ratio, 0), 1.0);
+    gl_FragColor = texture2D(src, shiftedCoord);
+}

bluerobotics/ping-viewer

qml/opengl/waterfallplot/fragment.glsl
@@ -1,0 +1,11 @@
+varying vec2 coord;
+uniform float horizontalRatio;
+uniform float verticalRatio;
+uniform sampler2D src;
+
+void main() {
+    // Mod is used to wrap around the 0-1 scaled x axis
+    vec2 shiftedCoord = mod(coord + vec2(horizontalRatio, 0), 1.0);
+    shiftedCoord.y = shiftedCoord.y * verticalRatio;
+    gl_FragColor = texture2D(src, shiftedCoord);
+}