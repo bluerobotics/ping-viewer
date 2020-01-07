patrickelectric/ping-components-1

Ping1DVisualizer: Use new WaterfallPlot with opengl
Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

qml/Ping1DVisualizer.qml
@@ -62,8 +62,20 @@ Item {
         orientation: Qt.Horizontal
         anchors.fill: parent
 
-        WaterfallPlot {
-            id: waterfall
+        ShaderEffect {
+            id: shader
+
+            WaterfallPlot {
+                id: waterfall
+                visible: false
+                anchors.fill: parent
+            }
+
+            property variant src: waterfall
+            property variant ratio: waterfall.drawRatio
+            vertexShader: "qrc:/opengl/waterfallplot/vertex.glsl"
+            fragmentShader: "qrc:/opengl/waterfallplot/fragment.glsl"
+
             Layout.fillHeight: true
             Layout.fillWidth: true
             Layout.preferredWidth: 350

bluerobotics/ping-viewer

qml/Ping1DVisualizer.qml
@@ -62,8 +62,21 @@ Item {
         orientation: Qt.Horizontal
         anchors.fill: parent
 
-        WaterfallPlot {
-            id: waterfall
+        ShaderEffect {
+            id: shader
+
+            WaterfallPlot {
+                id: waterfall
+                visible: false
+                anchors.fill: parent
+            }
+
+            property variant src: waterfall
+            property variant horizontalRatio: waterfall.drawHorizontalRatio
+            property variant verticalRatio: waterfall.drawVerticalRatio
+            vertexShader: "qrc:/opengl/waterfallplot/vertex.glsl"
+            fragmentShader: "qrc:/opengl/waterfallplot/fragment.glsl"
+
             Layout.fillHeight: true
             Layout.fillWidth: true
             Layout.preferredWidth: 350