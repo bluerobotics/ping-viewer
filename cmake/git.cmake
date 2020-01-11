97f26b941beb83b36cd4f329398f781d15b4c1cc
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-11 14:22:05 -0300

    CMakelists: Add and use git formula
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ cmake/git.cmake
@@ -0,0 +1,47 @@
+find_package(Git)
+
+if(NOT GIT_FOUND OR NOT EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    return()
+endif()
+
+# Update submodules as needed
+option(GIT_SUBMODULE "Check submodules during build" ON)
+if(NOT GIT_SUBMODULE)
+    return()
+endif()
+
+message(STATUS "Submodule update")
+execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                RESULT_VARIABLE GIT_SUBMODULE_RESULT)
+if(NOT GIT_SUBMODULE_RESULT EQUAL "0")
+    message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMODULE_RESULT}, please checkout submodules")
+endif()
+
+# Fetch the necessary git variables
+execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_VERSION
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%aI
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_VERSION_DATE
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+execute_process(COMMAND ${GIT_EXECUTABLE} describe --match "[v,t]*" --tags --abbrev=0
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_TAG
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+execute_process(COMMAND ${GIT_EXECUTABLE} remote get-url origin
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE GIT_URL
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+add_compile_definitions(
+    GIT_VERSION="${GIT_VERSION}"
+    GIT_VERSION_DATE="${GIT_VERSION_DATE}"
+    GIT_TAG="${GIT_TAG}"
+    GIT_URL="${GIT_URL}"
+)
