858c37144b93aec6e0c84ed77d00aab2ac5e6ef1
Patrick José Pereira <patrickelectric@gmail.com>
2020-09-25 10:50:44 -0300

    filelink: Handle real-time replay feature
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/link/filelink.cpp
+++ src/link/filelink.cpp
@@ -7,6 +7,7 @@
 
 #include "filelink.h"
 #include "logger.h"
+#include "settingsmanager.h"
 
 PING_LOGGING_CATEGORY(PING_PROTOCOL_FILELINK, "ping.protocol.filelink")
 
@@ -110,11 +111,23 @@ bool FileLink::startConnection()
 
     _processLog.reset(new ProcessLog());
     _processLog->moveToThread(&_processLogThread);
+
+    auto updateProcessReplayTime = [this](){
+        qDebug() << "Update replay time.";
+        int replayTimeMs = SettingsManager::self()->realTimeReplay() ? 0 : 100;
+        _processLog.get()->setReplayTimeMs(replayTimeMs);
+    };
+
+    updateProcessReplayTime();
+
     connect(&_processLogThread, &QThread::started, _processLog.get(), &ProcessLog::run);
     connect(&_processLogThread, &QThread::finished, _processLog.get(), &ProcessLog::stop);
     connect(_processLog.get(), &ProcessLog::newPackage, this, &FileLink::newData);
     connect(_processLog.get(), &ProcessLog::packageIndexChanged, this, &FileLink::packageIndexChanged);
     connect(_processLog.get(), &ProcessLog::packageIndexChanged, this, &FileLink::elapsedTimeChanged);
+    connect(SettingsManager::self(), &SettingsManager::realTimeReplayChanged, this, [updateProcessReplayTime]{
+        updateProcessReplayTime();
+    });
 
     processFile();
     return true;
