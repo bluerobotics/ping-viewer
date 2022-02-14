25592a186e55239be116c8dd1897dbb8f95eb314
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-14 14:19:36 -0300

    src: sensor: pingparserext: Update to use latest version of ping-cpp
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/sensor/pingparserext.cpp
+++ src/sensor/pingparserext.cpp
@@ -5,12 +5,12 @@ void PingParserExt::clearBuffer() { _parser.reset(); }
 void PingParserExt::parseBuffer(const QByteArray& data)
 {
     for (int i = 0; i < data.length(); i++) {
-        PingParser::ParseState state = _parser.parseByte(data.at(i));
-        if (state == PingParser::ParseState::NEW_MESSAGE) {
+        PingParser::State state = _parser.parseByte(data.at(i));
+        if (state == PingParser::State::NEW_MESSAGE) {
             parsed++;
             _rxMessage = _parser.rxMessage;
             emit newMessage(_rxMessage);
-        } else if (state == PingParser::ParseState::ERROR) {
+        } else if (state == PingParser::State::ERROR) {
             errors++;
             emit parseError();
         }
@@ -19,11 +19,11 @@ void PingParserExt::parseBuffer(const QByteArray& data)
 
 Parser::ParserState PingParserExt::parseByte(const char byte)
 {
-    PingParser::ParseState state = _parser.parseByte(byte);
-    if (state == PingParser::ParseState::NEW_MESSAGE) {
+    PingParser::State state = _parser.parseByte(byte);
+    if (state == PingParser::State::NEW_MESSAGE) {
         _rxMessage = _parser.rxMessage;
         return Parser::ParserState::NEW_MESSAGE;
-    } else if (state == PingParser::ERROR) {
+    } else if (state == PingParser::State::ERROR) {
         return Parser::ParserState::ERROR;
     }
 
